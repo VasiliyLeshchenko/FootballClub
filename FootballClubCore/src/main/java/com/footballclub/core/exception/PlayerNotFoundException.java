9d0e6af9dd8a3c3de53ace22169e7f7f495014cc
Leshchenko Vasily <leshchenkovasiliy17@gmail.com>
2024-04-08 11:39:12 +0300

    Added Exception Handler

--- FootballClubCore/src/main/java/com/footballclub/core/exception/PlayerNotFoundException.java
+++ FootballClubCore/src/main/java/com/footballclub/core/exception/PlayerNotFoundException.java
@@ -1,7 +1,15 @@
 package com.footballclub.core.exception;
 
-public class PlayerNotFoundException extends RuntimeException {
+public class PlayerNotFoundException extends NotFoundException {
     public PlayerNotFoundException(String message) {
         super(message);
+    }
+
+    public PlayerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PlayerNotFoundException(Throwable cause) {
+        super(cause);
     }
 }
--- FootballClubCore/src/main/java/com/footballclub/core/exception/PositionNotFoundException.java
+++ FootballClubCore/src/main/java/com/footballclub/core/exception/PositionNotFoundException.java
@@ -1,8 +1,16 @@
 package com.footballclub.core.exception;
 
-public class PositionNotFoundException extends RuntimeException {
+public class PositionNotFoundException extends NotFoundException {
 
     public PositionNotFoundException(String message) {
         super(message);
+    }
+
+    public PositionNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PositionNotFoundException(Throwable cause) {
+        super(cause);
     }
 }
